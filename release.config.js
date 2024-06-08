6a1052027ccf29ad69006a6e4c74c8c3f71b0607
Bernard Guiller Reccion <113100237+bernreccion@users.noreply.github.com>
2024-06-08 10:30:58 +0800

    chore: update release config

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ const config = {
         [
             '@semantic-release/commit-analyzer',
             {
-                preset: 'angular',
+                preset: 'conventionalcommits',
                 releaseRules: [
                     { type: "feat", release: "minor" },
                     { type: "fix", release: "patch" },
@@ -27,7 +27,14 @@ const config = {
         [
             "@semantic-release/release-notes-generator",
             {
-                preset: "angular"
+                preset: "conventionalcommits",
+                writerOpts: {
+                    groupBy: "type",
+                    commitGroupsSort: "title",
+                    commitsSort: ["type", "scope"],
+                    mainTemplate: "## BREAKING CHANGES\n{{#each commitGroups}}{{#if (eq this.title '⚠ BREAKING CHANGE')}}{{> commitGroup}}{{/if}}{{/each}}\n## FEATURES\n{{#each commitGroups}}{{#if (eq this.title 'feat')}}{{> commitGroup}}{{/if}}{{/each}}\n## PATCHES\n{{#each commitGroups}}{{#if (not (or (eq this.title 'BREAKING CHANGE') (eq this.title 'feat')) )}}{{> commitGroup}}{{/if}}{{/each}}",
+                    commitPartial: "- {{header}}{{#if notes}}\n  {{notes}}{{/if}}{{body}}\n",
+                }
             }
         ],
         [
@@ -39,6 +46,12 @@ const config = {
             '@semantic-release/git', {
                 assets: ['dist/*js', 'package.json', 'package-lock.json', 'docs/CHANGELOG.md'],
                 message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+        ],
+        [
+            "@semantic-release/npm",
+            {
+                npmPublish: false,
             }
         ],
         '@semantic-release/github'
