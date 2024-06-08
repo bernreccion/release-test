ead72d204f61de8332a6cb132b7e6a7c55ed0603
Bernard Guiller Reccion <113100237+bernreccion@users.noreply.github.com>
2024-06-08 08:56:56 +0800

    feat!: initial setup for semantic release

---
+++ release.config.js
@@ -0,0 +1,48 @@
+const config = {
+    branches: ['main'],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'conventionalcommits',
+                releaseRules: [
+                    { "type": "feat", "release": "minor" },
+                    { "type": "fix", "release": "patch" },
+                    { "type": "perf", "release": "patch" },
+                    { "type": "docs", "release": "patch" },
+                    { "type": "style", "release": "patch" },
+                    { "type": "refactor", "release": "patch" },
+                    { "type": "test", "release": "patch" },
+                    { "type": "build", "release": "patch" },
+                    { "type": "ci", "release": "patch" },
+                    { "type": "chore", "release": "patch" },
+                    { "type": "revert", "release": "patch" },
+                    { "breaking": true, "release": "major" }
+                ],
+                parserOpts: {
+                    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+                },
+            },
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                preset: "conventionalcommits"
+            }
+        ],
+        [
+            '@semantic-release/changelog', {
+                changelogFile: 'docs/CHANGELOG.md'
+            }
+        ],
+        [
+            '@semantic-release/git', {
+                assets: ['dist/*js', 'package.json', 'package-lock.json', 'docs/CHANGELOG.md'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+        ],
+        '@semantic-release/github'
+    ]
+}
+
+export default config
