5c9f4d78727490a016c90e43f4ca337d21f27e86
bernreccion <113100237+bernreccion@users.noreply.github.com>
2024-06-08 15:28:36 +0800

    Feat/test pr (#4)
    * chore: delete unused files
    
    * feat: test feature
    
    * ci: remove pull request trigger

--- helper.ts
+++
@@ -1,5 +0,0 @@
-import Handlebars from 'handlebars'
-
-Handlebars.registerHelper('eq', function(a: any, b: any, options: Handlebars.HelperOptions) {
-    return (a == b) ? options.fn(this) : options.inverse(this);
-});
