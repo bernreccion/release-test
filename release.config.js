e8b4d5316901e389d1b2b8cfe2ffadac077353ac
Bernard Guiller Reccion <113100237+bernreccion@users.noreply.github.com>
2024-06-08 10:57:39 +0800

    chore: update release config

--- release.config.js
+++ release.config.js
@@ -28,6 +28,20 @@ const config = {
             "@semantic-release/release-notes-generator",
             {
                 preset: "conventionalcommits",
+                presetConfig: {
+                    types: [
+                        { type: 'build', section: 'Build System', hidden: false },
+                        { type: 'chore', section: 'Build System', hidden: false },
+                        { type: 'ci', section: 'Continuous Integration', hidden: false },
+                        { type: 'docs', section: 'Documentation', hidden: false },
+                        { type: 'feat', section: 'Features', hidden: false },
+                        { type: 'fix', section: 'Bug Fixes', hidden: false },
+                        { type: 'perf', section: 'Performance Improvements', hidden: false },
+                        { type: 'refactor', section: 'Code Refactoring', hidden: false },
+                        { type: 'style', section: 'Styles', hidden: false },
+                        { type: 'test', section: 'Tests', hidden: false },
+                    ],
+                },
                 writerOpts: {
                     groupBy: "type",
                     commitGroupsSort: "title",
