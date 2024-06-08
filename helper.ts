94f04c38d678c4294a9bf5ddcf4b8b982038fc7e
Bernard Guiller Reccion <113100237+bernreccion@users.noreply.github.com>
2024-06-08 13:31:20 +0800

    chore: try again

--- helper.ts
+++ helper.ts
@@ -4,12 +4,12 @@ import Handlebars from 'handlebars'
 type EqualityHelper = (v1: any, v2: any, options: Handlebars.HelperOptions) => any;
 
 // Register the equality helper globally
-Handlebars.registerHelper('eq', function(v1: any, v2: any, options: Handlebars.HelperOptions) {
-  if (v1 === v2) {
-    return options.fn(this);
-  } else {
-    return options.inverse(this);
-  }
+Handlebars.registerHelper('eq', function (v1: any, v2: any, options: Handlebars.HelperOptions) {
+    if (v1 === v2) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
 } as EqualityHelper); // Type assertion to ensure the function matches the helper signature
 
 export default Handlebars;
