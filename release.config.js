dfb76b165bbc1a993eb36faf3a3ea8cc4a694d83
Bernard Guiller Reccion <113100237+bernreccion@users.noreply.github.com>
2024-06-08 11:28:54 +0800

    chore: update release

--- release.config.js
+++ release.config.js
@@ -55,12 +55,17 @@ const config = {
             }
         ],
         [
+            '@semantic-release/npm', {
+                npmPublish: false,
+                tarballDir: "dist"
+            }
+        ],
+        [
             '@semantic-release/git', {
                 assets: ['dist/*js', 'package.json', 'package-lock.json', 'docs/CHANGELOG.md'],
                 message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
             }
         ],
-        '@semantic-release/npm',
         '@semantic-release/github'
     ]
 }
